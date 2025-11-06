fa6b7937264912415faeb2bfce2113b600a0afc0
Dubey-Utkarsh1 <utkarshdubey025@gmail.com>
2025-11-06 09:12:52 +0530

    sedd chaange

--- seeders/seedAdmin.js
+++ seeders/seedAdmin.js
@@ -17,11 +17,11 @@ async function main() {
 
   // Upsert admin user
   const admin = await prisma.users.upsert({
-    where: {email: "adarsh@gmail.com"},
+    where: {email: "ashok@gmail.com"},
     update: {}, // don't overwrite existing user
     create: {
       role_id: role.id,
-      email: "adarsh@gmail.com",
+      email: "ashok@gmail.com",
       password: hashedPassword,
       fullname: "Admin",
       phone_number: "",
