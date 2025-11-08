f830a6f2f07419c7b3342da55e21b92228bccd05
Dubey-Utkarsh1 <utkarshdubey025@gmail.com>
2025-11-08 11:30:37 +0530

    cors changed

--- app.js
+++ app.js
@@ -26,30 +26,30 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// const corsOptions = {
-//   origin: "https://pms.trackable.in",
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true, // if you need to send cookies or auth headers
-// };
-
-// app.use(cors(corsOptions));
-
-const allowedOrigins = ["https://pms.trackable.in"];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://pms.trackable.in",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+  credentials: true, // if you need to send cookies or auth headers
 };
+
+app.use(cors(corsOptions));
+
+// const allowedOrigins = ["https://pms.trackable.in"];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// };
 app.use(cookieParser());
 // Session middleware setup
 app.use(
